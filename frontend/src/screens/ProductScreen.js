9c7a8265e79842bce90432e282149c6eff923af7
Trigonometri1 <56674309+Trigonometri1@users.noreply.github.com>
2023-03-23 20:28:27 +0300

    Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
+import products from "../products";
+import Rating from "../components/Rating";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 
 const ProductScreen = () => {
-  return <div>ProductScreen</div>;
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id);
+  console.log(product.name);
+
+  return <div>ProductScreen{product.name}</div>;
 };
 
 export default ProductScreen;
